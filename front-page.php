c9100f24e8c11e472f6e1ab8cfc11a8f0ce635e3
Shoaib Fateh <shoaibfateh21@yahoo.com>
2024-10-04 20:24:00 +0430

    implement experience section

--- front-page.php
+++ front-page.php
@@ -2,29 +2,102 @@
 
 <div class="section__hero">
     <div class="gp">
-    <div class="hero__title">
-        <div class="hero-title__left">
-            <div class="hero-title__bodine ">
-                <h1 class="hero-title-right hero-heading">BODINE<br>WERKT</h1>
+        <div class="hero__title">
+            <div class="hero-title__left">
+                <div class="hero-title__bodine ">
+                    <h1 class="hero-title-right hero-heading">BODINE<br>WERKT</h1>
 
-                <div class="ticker-wrapper">
-                    <div>
-                        <div class="text-block hero-heading rotatingText-adjective">TRANSPARANT</div>
-                        <div class="text-block hero-heading rotatingText-adjective">BETROUWBAAR</div>
-                        <div class="text-block hero-heading rotatingText-adjective">ENTHOUSIAST</div>
+                    <div class="ticker-wrapper">
+                        <div>
+                            <div class="text-block hero-heading rotatingText-adjective">TRANSPARANT</div>
+                            <div class="text-block hero-heading rotatingText-adjective">BETROUWBAAR</div>
+                            <div class="text-block hero-heading rotatingText-adjective">ENTHOUSIAST</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="hero__subtitle">
+                <p class="hero-subtitle">Werving &amp; Selectie<br>Executive Search<br>Recruitment Mediapakket</p>
+            </div>
+
+            <img src="<?php echo get_template_directory_uri(); ?>/images/intro.webp" loading="eager" width="786" height="857" alt="bodine buijsserd" class="intro-img">
+        </div>
+    </div>
+</div>
+
+
+
+<div class="wrap">
+    <div class="content content--sticky content--grid bg-1">
+        <div class="card-wrapper">
+            <div class="card-left">
+                <h2 class="h1 is-cards">WERVING&nbsp;<br>EN SELECTIE</h2>
+                <div class="tags-group">
+                    <div class="cards-label">+&nbsp;Kwalitatieve kandidaten</div>
+                    <div class="cards-label">+ Persoonlijke aanpak</div>
+                    <div class="cards-label">+&nbsp;Voordelig</div>
+                    <div class="cards-label">+ Transparante communicatie</div>
+                    <div class="cards-label">+&nbsp;Betrouwbaar</div>
+                </div>
+                <p class="paragraph is-1">Het werven en selecteren van de juiste kandidaten kost veel tijd! En door de huidige krapte op de arbeidsmarkt kost dit tegenwoordig nog meer tijd dan voorheen. Het kan dus geen kwaad om hulp te vragen bij een expert op dit gebied. Ik help je graag verder met het vinden van de juiste collega’s voor jullie organisatie! Mijn werkwijze is persoonlijk, transparant, voordelig en betrouwbaar.</p>
+            </div>
+            <div class="card-right">
+                <div class="card-image-wrapper">
+                    <div class="simpleParallax" style="overflow: hidden;">
+                        <img class="image-3 is-1" src="<?php echo get_template_directory_uri(); ?>/images/exp-1.webp" alt="image">
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 
-    <div class="hero__subtitle">
-        <p class="hero-subtitle">Werving &amp; Selectie<br>Executive Search<br>Recruitment Mediapakket</p>
+    <div class="content content--sticky content--grid bg-2">
+        <div class="card-wrapper">
+            <div class="card-left">
+                <h1 class="h1 is-2">EXECUTIVE<br>SEARCH A&nbsp;LA<br>BODINE</h1>
+                <div class="tags-group">
+                    <div class="cards-label">+ Hooggekwalificeerde kandidaten</div>
+                    <div class="cards-label">+ Senior en executive management</div>
+                    <div class="cards-label">+&nbsp;Diepgaand begrip van de industrie</div>
+                </div>
+                <p class="paragraph is-2">Executive search, ook wel ‘’headhunting’’ genoemd is een gespecialiseerde wervingsdienst waarbij ik mij meer richt op het vinden van hooggekwalificeerde kandidaten voor senior- en executive-level posities binnen jullie organisatie. Dit proces vereist een diepgaand begrip van de industrie en de specifieke behoeften van jullie organisatie, de vaardigheden en kwaliteiten die nodig zijn voor leiderschapsrollen.</p><a href="#Footer" class="main-button is-secondary w-button">Contact opnemen</a>
+            </div>
+            <div class="card-right">
+                <div class="card-image-wrapper">
+                    <div class="simpleParallax" style="overflow: hidden;">
+                        <img class="image-3 is-1" src="<?php echo get_template_directory_uri(); ?>/images/exp-2.webp" alt="image">
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 
-    <img src="<?php echo get_template_directory_uri(); ?>/images/intro.webp" loading="eager" width="786" height="857" alt="bodine buijsserd" class="intro-img">
-</div>
-</div>
+    <div class="content content--sticky content--grid bg-3">
+        <div class="card-wrapper">
+            <div class="card-left">
+                <h1 class="h1 is-3">RECRUITMENT&nbsp;<br>VIDEOS&nbsp;DIE&nbsp;<br>AANSPREKEN</h1>
+                <div class="tags-group">
+                    <div class="cards-label is-3">+ Professionele branding</div>
+                    <div class="cards-label is-3">+ Meetbare resultaten</div>
+                    <div class="cards-label is-3">+ Betere engagement</div>
+                    <div class="cards-label is-3">+ Heldere communicatie</div>
+                    <div class="cards-label is-3">+ Persoonlijke benadering</div>
+                </div>
+                <p class="paragraph is-3">
+                    Door te kiezen voor 
+                    <span class="text-span-3">werving en selectie</span> + een <span class="text-span-4">op maat gemaakt mediapakket</span>, kies je voor een gerichte campagne die specifiek zijn afgestemd op het aantrekken van talenten die perfect passen bij jullie bedrijfscultuur en de eisen van de vacature. Dit leidt tot een hogere kwaliteit van kandidaten, waardoor je tijd en middelen bespaart op het wervingsproces. Video content is dé manier om jouw boodschap krachtig over te brengen. Je krijgt meer <span class="text-span-5">gekwalificeerde leads</span> die echt interesse hebben in jullie vacatures en organisatie.<br></p>
+                    <a href="#Footer" class="main-button w-button">Contact opnemen</a>
+            </div>
+            <div class="card-right">
+                <div class="card-image-wrapper">
+                    <div class="simpleParallax" style="overflow: hidden;">
+                        <img class="image-3 is-1" src="<?php echo get_template_directory_uri(); ?>/images/exp-2.webp" alt="image">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
 <?php get_footer(); ?>
- 
