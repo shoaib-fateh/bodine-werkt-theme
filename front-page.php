44313eb3e9a7ada18f52cfd2af9c28a9e7c288ee
Shoaib Fateh <shoaibfateh21@yahoo.com>
2024-10-05 21:48:24 +0430

    update some funs

--- front-page.php
+++ front-page.php
@@ -49,7 +49,7 @@ get_header(); ?>
                 </div>
             </div>
             <div data-w-id="0110726c-470c-8f3c-0f95-8f423fba65d3" class="werkplezier-right">
-                <div data-w-id="78556121-44b0-0fd1-6dd1-dd1e18042ddb" class="cursor-wraper" style="">
+                <!-- <div data-w-id="78556121-44b0-0fd1-6dd1-dd1e18042ddb" class="cursor-wraper" style="">
                     <div class="cursor-circle">
                         <div data-w-id="2c9aa2e5-9298-98ba-ff63-43f6968708e7" class="mute-embed w-embed" style="display: none;"><svg id="mute-svg" width="24px" height="24px" viewBox="0 0 24 24" data-volume-muted-icon="true">
                                 <path fill="white" fill-rule="evenodd" clip-rule="evenodd" d="M14 2.20001C15.1263 2.42863 16.1838 2.84654 17.1379 3.41908L15.5513 4.82943C15.0606 4.58595 14.5414 4.39136 14 4.25201V2.20001ZM21.1249 7.90287L19.5378 9.31361C19.8371 10.1532 20 11.0576 20 12C20 15.7277 17.4505 18.8599 14 19.7479V21.7999C18.5645 20.8734 22 16.8379 22 12C22 10.5401 21.6872 9.15325 21.1249 7.90287ZM12 4.00396V7.98614L3.17811 15.8278C2.48346 15.5143 2 14.8156 2 14.004V10.004C2 8.89939 2.89543 8.00396 4 8.00396H6.58579L10.2929 3.29685C10.9229 2.66689 12 3.11305 12 4.00396ZM12 10.662L5.99037 16.004L2.33565 19.2526C1.92286 19.6195 1.88568 20.2516 2.2526 20.6644C2.61952 21.0772 3.25159 21.1143 3.66437 20.7474L21.6644 4.74742C22.0772 4.3805 22.1143 3.74843 21.7474 3.33565C21.3805 2.92286 20.7484 2.88568 20.3356 3.2526L12 10.662ZM10.2929 20.7111L8.81985 18.8407L12 16.0139V20.004C12 20.8949 10.9229 21.341 10.2929 20.7111ZM14 15.4648C15.0633 14.8498 15.8172 13.7593 15.971 12.4841L17.8778 10.7892C17.9579 11.1803 18 11.5852 18 12C18 14.6124 16.3304 16.8349 14 17.6586V15.4648Z"></path>
@@ -58,7 +58,7 @@ get_header(); ?>
                                 <path fill="white" fill-rule="evenodd" clip-rule="evenodd" d="M20 12C20 15.7277 17.4505 18.8599 14 19.7479V21.7999C18.5645 20.8734 22 16.8379 22 12C22 7.16206 18.5645 3.12655 14 2.20001V4.25201C17.4505 5.1401 20 8.2723 20 12ZM18 12C18 9.38754 16.3304 7.16506 14 6.34139V8.53511C15.1956 9.22672 16 10.5194 16 12C16 13.4805 15.1956 14.7732 14 15.4648V17.6586C16.3304 16.8349 18 14.6124 18 12ZM6.58579 8.00396H4C2.89543 8.00396 2 8.89939 2 10.004V14.004C2 15.1085 2.89543 16.004 4 16.004H6.58579L10.2929 20.7111C10.9229 21.341 12 20.8949 12 20.004V4.00396C12 3.11305 10.9229 2.66689 10.2929 3.29685L6.58579 8.00396Z"></path>
                             </svg></div>
                     </div>
-                </div>
+                </div> -->
                 <div class="circle-text-wrap" style="translate: none; rotate: none; scale: none; transform: translate3d(0px, 0px, 0px) rotate(-233.923deg);">
                     <div class="unmuted">
                         <div class="circle-text">
@@ -66,13 +66,30 @@ get_header(); ?>
                         </div>
                     </div>
                 </div>
-                <div class="code-embed w-embed">
-                    <div class="video-container" style="position: relative; width: 100%; height: 100%;">
-                        <video id="wb-video" class="wb-video-styles-controller" style="height:100%; width:100%; border-radius:2rem;" playsinline="" autoplay="" muted="" loop="">
-                            <source src="<?php echo get_template_directory_uri(); ?>/images/about.mp4">
-                        </video>
-                    </div>
-                </div>
+                <div class="video-container" style="position: relative; width: 100%; height: 100%;">
+  <video id="wb-video" class="wb-video-styles-controller" style="height:100%; width:100%; border-radius:2rem;" playsinline="" autoplay="" muted="" loop="">
+      <source src="<?php echo get_template_directory_uri(); ?>/images/about.mp4">
+  </video>
+
+  <!-- Mute/Unmute Button -->
+  <div id="mute-button" class="cursor-wraper">
+      <div class="cursor-circle">
+          <!-- Mute Icon (default visible) -->
+          <div id="mute-icon" class="mute-embed w-embed" style="display: flex;">
+            <svg id="mute-svg" width="24px" height="24px" viewBox="0 0 24 24" data-volume-icon="true">
+              <path fill="white" fill-rule="evenodd" clip-rule="evenodd" d="M20 12C20 15.7277 17.4505 18.8599 14 19.7479V21.7999C18.5645 20.8734 22 16.8379 22 12C22 7.16206 18.5645 3.12655 14 2.20001V4.25201C17.4505 5.1401 20 8.2723 20 12ZM18 12C18 9.38754 16.3304 7.16506 14 6.34139V8.53511C15.1956 9.22672 16 10.5194 16 12C16 13.4805 15.1956 14.7732 14 15.4648V17.6586C16.3304 16.8349 18 14.6124 18 12ZM6.58579 8.00396H4C2.89543 8.00396 2 8.89939 2 10.004V14.004C2 15.1085 2.89543 16.004 4 16.004H6.58579L10.2929 20.7111C10.9229 21.341 12 20.8949 12 20.004V4.00396C12 3.11305 10.9229 2.66689 10.2929 3.29685L6.58579 8.00396Z"></path>
+            </svg>
+          </div>
+          <!-- Unmute Icon (hidden by default) -->
+          <div id="unmute-icon" class="unmute-embed w-embed" style="display: none;">
+            <svg id="unmute-svg" width="24px" height="24px" viewBox="0 0 24 24" data-volume-muted-icon="true">
+              <path fill="white" fill-rule="evenodd" clip-rule="evenodd" d="M14 2.20001C15.1263 2.42863 16.1838 2.84654 17.1379 3.41908L15.5513 4.82943C15.0606 4.58595 14.5414 4.39136 14 4.25201V2.20001ZM21.1249 7.90287L19.5378 9.31361C19.8371 10.1532 20 11.0576 20 12C20 15.7277 17.4505 18.8599 14 19.7479V21.7999C18.5645 20.8734 22 16.8379 22 12C22 10.5401 21.6872 9.15325 21.1249 7.90287ZM12 4.00396V7.98614L3.17811 15.8278C2.48346 15.5143 2 14.8156 2 14.004V10.004C2 8.89939 2.89543 8.00396 4 8.00396H6.58579L10.2929 3.29685C10.9229 2.66689 12 3.11305 12 4.00396ZM12 10.662L5.99037 16.004L2.33565 19.2526C1.92286 19.6195 1.88568 20.2516 2.2526 20.6644C2.61952 21.0772 3.25159 21.1143 3.66437 20.7474L21.6644 4.74742C22.0772 4.3805 22.1143 3.74843 21.7474 3.33565C21.3805 2.92286 20.7484 2.88568 20.3356 3.2526L12 10.662ZM10.2929 20.7111L8.81985 18.8407L12 16.0139V20.004C12 20.8949 10.9229 21.341 10.2929 20.7111ZM14 15.4648C15.0633 14.8498 15.8172 13.7593 15.971 12.4841L17.8778 10.7892C17.9579 11.1803 18 11.5852 18 12C18 14.6124 16.3304 16.8349 14 17.6586V15.4648Z"></path>
+            </svg>
+          </div>
+      </div>
+  </div>
+</div>
+
             </div>
         </div>
     </div>
--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Rotating adjectives every 2 seconds
 const adjectives = document.querySelectorAll('.rotatingText-adjective');
 let currentIndex = 0;
 
@@ -12,29 +13,74 @@ function rotateAdjectives() {
   currentIndex = (currentIndex + 1) % adjectives.length;
 }
 
-// Rotate every 3 seconds
+// Rotate adjectives every 2 seconds
 setInterval(rotateAdjectives, 2000);
 
 
-
+// jQuery functions for hamburger menu and smooth scroll
 jQuery(document).ready(function($) {
+  // Hamburger menu toggle
   $(".hamburger").on("click", function() {
-      if ($(".mobile-navbar").hasClass("active")) {
-          $(".mobile-navbar").slideUp(500, function() {
-              $(this).removeClass("active");
-          });
-      } else {
-          $(".mobile-navbar").slideDown(500).addClass("active");
-      }
+    if ($(".mobile-navbar").hasClass("active")) {
+      $(".mobile-navbar").slideUp(500, function() {
+        $(this).removeClass("active");
+      });
+    } else {
+      $(".mobile-navbar").slideDown(500).addClass("active");
+    }
   });
 
+  // Smooth scroll for anchor links
   $('a[href*="#"]').on('click', function(event) {
     event.preventDefault();
 
     $('html, body').animate({
-        scrollTop: $($(this).attr('href')).offset().top
+      scrollTop: $($(this).attr('href')).offset().top
     }, 500); // 500 milliseconds for smooth scroll
-});
+  });
+
+  // Scroll-based circle rotation (slower rotation)
+  $(window).on('scroll', function() {
+    // Get the current scroll position
+    var scrollPosition = $(this).scrollTop();
+
+    // Make the rotation slower by dividing the scrollPosition by 4
+    var rotation = (scrollPosition / 4) % 360;
+
+    // Apply the slower rotation to the circle element
+    $('.circle-text-wrap').css({
+      'transform': 'rotate(' + rotation + 'deg)'
+    });
+  });
 });
 
+// Variables
+const videoContainer = document.querySelector('.video-container');
+const muteButton = document.getElementById('mute-button');
+const video = document.getElementById('wb-video');
+const muteIcon = document.getElementById('mute-icon');
+const unmuteIcon = document.getElementById('unmute-icon');
 
+// Track mouse movement and set button position relative to video container
+videoContainer.addEventListener('mousemove', (e) => {
+    const rect = videoContainer.getBoundingClientRect();
+    const x = e.clientX - rect.left - (muteButton.offsetWidth / 2) + 10;  // Center the button on X
+    const y = e.clientY - rect.top - (muteButton.offsetHeight / 2) + 10;  // Center the button on Y and move it down by 10px
+
+    // Set the button's position
+    muteButton.style.left = `${x}px`;
+    muteButton.style.top = `${y}px`;
+});
+
+// Toggle sound on click
+muteButton.addEventListener('click', () => {
+    if (video.muted) {
+        video.muted = false;
+        muteIcon.style.display = 'none';
+        unmuteIcon.style.display = 'flex';
+    } else {
+        video.muted = true;
+        muteIcon.style.display = 'flex';
+        unmuteIcon.style.display = 'none';
+    }
+});
