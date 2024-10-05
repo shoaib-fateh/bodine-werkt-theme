b7e25ace15ffa81b10b9ebecffa83dafeaf33301
Shoaib Fateh <shoaibfateh21@yahoo.com>
2024-10-05 19:00:28 +0430

    update the code

--- single-blogs.php
+++ single-blogs.php
@@ -21,7 +21,7 @@ if ( have_posts() ) :
             <div class="padding-global">
                 <div class="container">
                     <div class="section-headding-area">
-                        <h1 class="h1 vacatures"><?php echo esc_html($blog_title); ?></h1>
+                        <h2 class="h2 vacatures"><?php echo esc_html($blog_title); ?></h2>
                         <p class="hero-subtitle--vacatures">Hier deel ik mijn ideeën, ervaringen en andere persoonlijke insights in mijn werk en leven. Bij vragen of reacties mag je altijd contact opnemen!</p>
                     </div>
                 </div>
@@ -55,28 +55,35 @@ if ( have_posts() ) :
                         <div class="similar-jobs">
                             <div class="txt-details">Featured</div>
                             <div class="w-dyn-list">
-                                <div role="list" class="simililar-job-list w-dyn-items">
-                                    <div role="listitem" class="w-dyn-item"><a href="/vacatures/medewerker-contactcenter" class="text-decoration-none w-inline-block">
-                                            <div class="similar-job-item-wrapper">
-                                                <div class="similar-job-list-image-wrapper"><img src="https://cdn.prod.website-files.com/66756e96daf5ec4e9f4d9425/66c60652af563308863a16a5_Austin%20Distel%20Unsplash.webp" loading="lazy" alt="" sizes="(max-width: 479px) 87vw, (max-width: 767px) 23vw, (max-width: 991px) 18vw, 11vw" srcset="https://cdn.prod.website-files.com/66756e96daf5ec4e9f4d9425/66c60652af563308863a16a5_Austin%20Distel%20Unsplash-p-500.webp 500w, https://cdn.prod.website-files.com/66756e96daf5ec4e9f4d9425/66c60652af563308863a16a5_Austin%20Distel%20Unsplash-p-800.webp 800w, https://cdn.prod.website-files.com/66756e96daf5ec4e9f4d9425/66c60652af563308863a16a5_Austin%20Distel%20Unsplash-p-1080.webp 1080w, https://cdn.prod.website-files.com/66756e96daf5ec4e9f4d9425/66c60652af563308863a16a5_Austin%20Distel%20Unsplash-p-1600.webp 1600w, https://cdn.prod.website-files.com/66756e96daf5ec4e9f4d9425/66c60652af563308863a16a5_Austin%20Distel%20Unsplash-p-2000.webp 2000w, https://cdn.prod.website-files.com/66756e96daf5ec4e9f4d9425/66c60652af563308863a16a5_Austin%20Distel%20Unsplash-p-2600.webp 2600w, https://cdn.prod.website-files.com/66756e96daf5ec4e9f4d9425/66c60652af563308863a16a5_Austin%20Distel%20Unsplash-p-3200.webp 3200w, https://cdn.prod.website-files.com/66756e96daf5ec4e9f4d9425/66c60652af563308863a16a5_Austin%20Distel%20Unsplash.webp 4876w" class="similar-job-list-image"></div>
-                                                <div class="txt-details">Medewerker Contactcenter</div>
+                            <div role="list" class="featured-blog-list w-dyn-items">
+                                    <?php 
+                                    // Query for three random blogs
+                                    $args = array(
+                                        'post_type' => 'blogs', // Change to your custom post type 'blog'
+                                        'posts_per_page' => 3,
+                                        'orderby' => 'rand', // Order by random
+                                    );
+
+                                    $random_query = new WP_Query($args);
+
+                                    if ($random_query->have_posts()) :
+                                        while ($random_query->have_posts()) : $random_query->the_post(); ?>
+                                            <div role="listitem" class="w-dyn-item">
+                                                <a href="<?php the_permalink(); ?>" class="featured-blog-item-link w-inline-block">
+                                                    <div class="featured-blog-item-wrapper">
+                                                        <div class="similar-job-list-image-wrapper">
+                                                            <img src="<?php echo esc_url(get_field('photo')); ?>" loading="lazy" alt="<?php the_title(); ?>" class="similar-job-list-image">
+                                                        </div>
+                                                        <div class="head-form cc-margin"><?php the_title(); ?></div>
+                                                    </div>
+                                                </a>
                                             </div>
-                                        </a></div>
-                                    <div role="listitem" class="w-dyn-item"><a href="/vacatures/financieel-administratief-medewerker" class="text-decoration-none w-inline-block">
-                                            <div class="similar-job-item-wrapper">
-                                                <div class="similar-job-list-image-wrapper"><img src="https://cdn.prod.website-files.com/66756e96daf5ec4e9f4d9425/66c605d0c7da6a7cd407a61b_Emma%20dau%20unsplash.webp" loading="lazy" alt="" sizes="(max-width: 479px) 87vw, (max-width: 767px) 23vw, (max-width: 991px) 18vw, 11vw" srcset="https://cdn.prod.website-files.com/66756e96daf5ec4e9f4d9425/66c605d0c7da6a7cd407a61b_Emma%20dau%20unsplash-p-500.webp 500w, https://cdn.prod.website-files.com/66756e96daf5ec4e9f4d9425/66c605d0c7da6a7cd407a61b_Emma%20dau%20unsplash-p-800.webp 800w, https://cdn.prod.website-files.com/66756e96daf5ec4e9f4d9425/66c605d0c7da6a7cd407a61b_Emma%20dau%20unsplash-p-1080.webp 1080w, https://cdn.prod.website-files.com/66756e96daf5ec4e9f4d9425/66c605d0c7da6a7cd407a61b_Emma%20dau%20unsplash-p-1600.webp 1600w, https://cdn.prod.website-files.com/66756e96daf5ec4e9f4d9425/66c605d0c7da6a7cd407a61b_Emma%20dau%20unsplash-p-2000.webp 2000w, https://cdn.prod.website-files.com/66756e96daf5ec4e9f4d9425/66c605d0c7da6a7cd407a61b_Emma%20dau%20unsplash-p-2600.webp 2600w, https://cdn.prod.website-files.com/66756e96daf5ec4e9f4d9425/66c605d0c7da6a7cd407a61b_Emma%20dau%20unsplash-p-3200.webp 3200w, https://cdn.prod.website-files.com/66756e96daf5ec4e9f4d9425/66c605d0c7da6a7cd407a61b_Emma%20dau%20unsplash.webp 6720w" class="similar-job-list-image"></div>
-                                                <div class="txt-details">Financieel Administratief Medewerker</div>
-                                            </div>
-                                        </a></div>
-                                    <div role="listitem" class="w-dyn-item"><a href="/vacatures/commercieel-medewerker-binnendienst-ict" class="text-decoration-none w-inline-block">
-                                            <div class="similar-job-item-wrapper">
-                                                <div class="similar-job-list-image-wrapper"><img src="https://cdn.prod.website-files.com/66756e96daf5ec4e9f4d9425/66c6025bff06b5f000b5da9d_Brooke%20Cagle%20Unsplash.webp" loading="lazy" alt="" sizes="(max-width: 479px) 87vw, (max-width: 767px) 23vw, (max-width: 991px) 18vw, 11vw" srcset="https://cdn.prod.website-files.com/66756e96daf5ec4e9f4d9425/66c6025bff06b5f000b5da9d_Brooke%20Cagle%20Unsplash-p-500.webp 500w, https://cdn.prod.website-files.com/66756e96daf5ec4e9f4d9425/66c6025bff06b5f000b5da9d_Brooke%20Cagle%20Unsplash-p-800.webp 800w, https://cdn.prod.website-files.com/66756e96daf5ec4e9f4d9425/66c6025bff06b5f000b5da9d_Brooke%20Cagle%20Unsplash-p-1080.webp 1080w, https://cdn.prod.website-files.com/66756e96daf5ec4e9f4d9425/66c6025bff06b5f000b5da9d_Brooke%20Cagle%20Unsplash-p-1600.webp 1600w, https://cdn.prod.website-files.com/66756e96daf5ec4e9f4d9425/66c6025bff06b5f000b5da9d_Brooke%20Cagle%20Unsplash-p-2000.webp 2000w, https://cdn.prod.website-files.com/66756e96daf5ec4e9f4d9425/66c6025bff06b5f000b5da9d_Brooke%20Cagle%20Unsplash-p-2600.webp 2600w, https://cdn.prod.website-files.com/66756e96daf5ec4e9f4d9425/66c6025bff06b5f000b5da9d_Brooke%20Cagle%20Unsplash-p-3200.webp 3200w, https://cdn.prod.website-files.com/66756e96daf5ec4e9f4d9425/66c6025bff06b5f000b5da9d_Brooke%20Cagle%20Unsplash.webp 6547w" class="similar-job-list-image"></div>
-                                                <div class="txt-details">Commercieel medewerker binnendienst ICT</div>
-                                            </div>
-                                        </a></div>
+                                        <?php endwhile; ?>
+                                    <?php else : ?>
+                                        <p><?php esc_html_e('Sorry, no blogs available.', 'textdomain'); ?></p>
+                                    <?php endif; ?>
                                 </div>
                             </div>
-                            <div class="button-area block"><a href="mailto:info@bodinewerkt.nl" class="main-button w-button">MAIL&nbsp;BODINE</a><a href="tel:+31657757947" class="main-button is-outline w-button">Bel bodine</a></div>
                         </div>
                     </div>
                 </div>
